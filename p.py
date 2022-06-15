3ad9c7d8bfc23683ca3bf552700587f243cdbbd1
Moaed-khola <101525274+Moaed-khola@users.noreply.github.com>
2022-06-15 05:54:50 +0300

    sdfsdfsdf

--- p.py
+++ p.py
@@ -48,3 +48,7 @@ Please type Y for YES or N for NO.
 calculate()
 print('See you later.')
 print('See you later...')
+print('See you later...')
+print('See you later...')
+print('See you later...')
+
